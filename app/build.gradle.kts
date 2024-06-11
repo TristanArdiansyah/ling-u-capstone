2df7eb0b781b8d1798656e7bd84711ef6541d3fa
Your Namegit <you@example.com>
2024-06-11 09:39:58 +0700

    feat:add all course, profile activity, and move the logout button to profile page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,12 +39,14 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
     implementation("com.android.identity:identity-credential:20231002")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -57,4 +59,7 @@ dependencies {
     implementation("androidx.credentials:credentials:1.0.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.0.0")
     implementation("com.google.android.gms:play-services-auth-api-phone:17.5.0")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.0.4")
+
 }
---
+++ app/src/main/java/com/bangkit/capstone/lingu/profile/ProfileActivity.kt
@@ -0,0 +1,101 @@
+package com.bangkit.capstone.lingu.profile
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.credentials.ClearCredentialStateRequest
+import androidx.credentials.CredentialManager
+import androidx.lifecycle.lifecycleScope
+import com.bangkit.capstone.lingu.R
+import com.bangkit.capstone.lingu.course.AllCourseActivity
+import com.bangkit.capstone.lingu.databinding.ActivityAllCourseBinding
+import com.bangkit.capstone.lingu.databinding.ActivityProfileBinding
+import com.bangkit.capstone.lingu.view.ViewModelFactory
+import com.bangkit.capstone.lingu.view.login.LoginActivity
+import com.bangkit.capstone.lingu.view.main.MainActivity
+import com.bangkit.capstone.lingu.view.main.MainViewModel
+import com.bangkit.capstone.lingu.view.welcome.WelcomeActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.launch
+
+class ProfileActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+    private lateinit var binding: ActivityProfileBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.getSession().observe(this) { user ->
+            if (!user.isLogin) {
+                startActivity(Intent(this, WelcomeActivity::class.java))
+                finish()
+
+            }
+
+            auth = Firebase.auth
+            val firebaseUser = auth.currentUser
+            if (firebaseUser == null) {
+                // Not signed in, launch the Login activity
+                startActivity(Intent(this, WelcomeActivity::class.java))
+                finish()
+            }
+
+        }
+
+        setupAction()
+        playAnimation()
+    }
+
+    private fun setupAction(){
+        binding.logoutButton.setOnClickListener {
+            lifecycleScope.launch {
+                val credentialManager = CredentialManager.create(this@ProfileActivity)
+                auth.signOut()
+                credentialManager.clearCredentialState(ClearCredentialStateRequest())
+                startActivity(Intent(this@ProfileActivity, LoginActivity::class.java))
+                finish()
+            }
+            viewModel.logout()
+        }
+
+        binding.homeButtonNonFilled.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.searchButtonNonFill.setOnClickListener {
+            val intent = Intent(this, AllCourseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val logout = ObjectAnimator.ofFloat(binding.logoutButton, View.ALPHA, 1f).setDuration(100)
+
+        AnimatorSet().apply {
+            playSequentially(logout)
+            startDelay = 100
+        }.start()
+    }
+}
